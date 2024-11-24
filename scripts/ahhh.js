49eb9fe73c8a7b1636c68a9eafd228ab2bb09c3a
pau <paul.e.soukup@gmail.com>
2024-11-24 12:46:37 +0100

    removed unused file

--- scripts/ahhh.js
+++
@@ -1,7 +0,0 @@
-
-
-export function getout() {
-    const getouturl = "https://i-viaplay-com.akamaized.net/viaplay-prod/22/792/1504101650-d9ec8b24ef8005710f40f16ba090ea1db9d29e29.jpg?width=1600&height=900"
-    window.location.href = getouturl;
-}
-setInterval(getout, 10);
